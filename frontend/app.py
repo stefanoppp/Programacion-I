0be885c45d8d2658135c0b7906edda9291abdaf7
EnzoPonce <enzoponce21@gmail.com>
2022-03-21 07:07:11 -0300

    estructura aplicacion

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,6 @@
-from flask import Flask
 import os
-from dotenv import load_dotenv
-load_dotenv()
-app = Flask(__name__)
+from main import create_app
+app = create_app()
+app.app_context().push()
 if __name__=='__main__':
 	app.run(port=os.getenv("PORT"),debug=True)
