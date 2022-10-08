779a321309ce938bce6f002c6ad0e4420cd5c02d
chuli99 <juliancastillo0399@gmail.com>
2022-10-08 20:21:29 -0300

    tratando de solucionar error

--- PoemasApp/src/app/app-routing.module.ts
+++ PoemasApp/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PoemasComponent } from './pages/poemas/poemas.component';
+import { DashboardAdminComponent } from './pages/dashboard-admin/dashboard-admin.component';
+import { DashboardUserComponent } from './pages/dashboard-user/dashboard-user.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '' , component: HomeComponent},
+  { path: 'login' , component: LoginComponent},
+  { path: 'poemas' , component: PoemasComponent},
+  { path: 'dashboard-admin', component:DashboardAdminComponent},
+  { path: 'dashboard-user', component: DashboardUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
