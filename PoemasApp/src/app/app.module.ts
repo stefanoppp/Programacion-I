153dc9bf20c7c9c5a647c4b22351e0d300253e5e
francisco espinola <franespinola_92@hotmail.com>
2022-10-08 14:02:07 -0300

    agregue componentes

--- PoemasApp/src/app/app.module.ts
+++ PoemasApp/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ComponentsComponent } from './components/components/components.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardUserComponent } from './pages/dashboard-user/dashboard-user.component';
+import { DashboardAdminComponent } from './pages/dashboard-admin/dashboard-admin.component';
+import { PoemasComponent } from './pages/poemas/poemas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ComponentsComponent,
+    CardsComponent,
+    LoginComponent,
+    DashboardUserComponent,
+    DashboardAdminComponent,
+    PoemasComponent
   ],
   imports: [
     BrowserModule,
---
+++ PoemasApp/src/app/pages/poemas/poemas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poemas',
+  templateUrl: './poemas.component.html',
+  styleUrls: ['./poemas.component.css']
+})
+export class PoemasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
