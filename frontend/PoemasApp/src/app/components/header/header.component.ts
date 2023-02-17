f007d080fbfd92578d4ae912b42db6d2311a1555
franespinola <franespinola_92@hotmail.com>
2023-02-16 22:07:43 -0300

    implemente el buscador en el componente

--- frontend/PoemasApp/src/app/components/header/header.component.ts
+++ frontend/PoemasApp/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth1Service } from 'src/app/service/auth1.service';
+import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,12 @@ import { Auth1Service } from 'src/app/service/auth1.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  usuarios: any[] = [];
+
   constructor(
-    private authService:Auth1Service
+    private authService:Auth1Service,
+    private usuarioService: UsuarioService
   ) { }
 
   ngOnInit(): void {
@@ -21,6 +26,12 @@ export class HeaderComponent implements OnInit {
   }
   get isAdmin(){
     return localStorage.getItem("admin") ==='true'?true:false || undefined
+  }
+  buscarUsuarios(termino:string){
+     this.usuarioService.buscarUsuarios(termino).subscribe((data:any) => {
+      console.log(data);
+      this.usuarios = data.usuarios // Asignamos los usuarios recibidos desde el servicio a la propiedad 'usuarios'
+    });
   }
 
 }
